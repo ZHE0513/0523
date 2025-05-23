786664b75e210f425ffdb048fdd80706a8984dea
ZHE0513 <413730721@o365.tku.edu.tw>
2025-05-23 11:40:44 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,6 +6,8 @@ const newPoints = [76, 77, 90, 180, 85, 16, 315, 404, 320, 307, 306, 408, 304, 3
 const leftEye1 = [243, 190, 56, 28, 27, 29, 30, 247, 130, 25, 110, 24, 23, 22, 26, 112];
 const leftEye2 = [133, 173, 157, 158, 159, 160, 161, 246, 33, 7, 163, 144, 145, 153, 154, 155];
 const rightEye = [359, 467, 260, 259, 257, 258, 286, 414, 463, 341, 256, 252, 253, 254, 339, 255];
+const rightEye1 = [359, 467, 260, 259, 257, 258, 286, 414, 463, 341, 256, 252, 253, 254, 339, 255];
+const rightEye2 = [263, 466, 388, 387, 386, 385, 384, 398, 362, 382, 381, 380, 374, 373, 390, 249];
 
 function setup() {
   createCanvas(640, 480);
@@ -66,6 +68,20 @@ function draw() {
     for (let i = 0; i < rightEye.length - 1; i++) {
       const [x1, y1] = keypoints[rightEye[i]];
       const [x2, y2] = keypoints[rightEye[i + 1]];
+      line(x1, y1, x2, y2);
+    }
+
+    // Draw lines connecting points in the rightEye1 array
+    for (let i = 0; i < rightEye1.length - 1; i++) {
+      const [x1, y1] = keypoints[rightEye1[i]];
+      const [x2, y2] = keypoints[rightEye1[i + 1]];
+      line(x1, y1, x2, y2);
+    }
+
+    // Draw lines connecting points in the rightEye2 array
+    for (let i = 0; i < rightEye2.length - 1; i++) {
+      const [x1, y1] = keypoints[rightEye2[i]];
+      const [x2, y2] = keypoints[rightEye2[i + 1]];
       line(x1, y1, x2, y2);
     }
   }
